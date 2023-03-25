ec15cd625933d6933d1178c31c8ef206a3d3a88b
ying.zhang <ying.zhang_zhang@neusoft.com>
2023-03-26 00:17:11 +0800

    feat: react-router-dom集成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navbar/navbar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+        <div>
+          <NavBar/>
+          <main className="container">
+              <Switch >
+                  <Route path="/movies" component={Movies} />
+              </Switch>
+          </main>
+        </div>
     );
   }
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/movies">Movies</Link>
+      </li>
+      <li>
+        <Link to="/posts/2020/06">Posts</Link>
+      </li>
+      <li>
+        <Link to="/admin">Admin</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
